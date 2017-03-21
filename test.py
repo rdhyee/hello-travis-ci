61d3b8b2c9d83d25dd21466eb27a749371b002af
Raymond Yee <raymond.yee@gmail.com>
2017-03-21 14:45:29 -0700

    first pass at Selenium test

--- test.py
+++ test.py
@@ -7,11 +7,23 @@ from builtins import (bytes, str, open, super, range,
 import unittest
 import requests
 
+import selenium
+from selenium import webdriver
+
+
 class RquestTestCase(unittest.TestCase):
     def test_request(self):
         
         r = requests.get("https://github.com")
         self.assertEqual(r.status_code, 200)
+
+    def test_selenium(self):
+
+    	sel = webdriver.Firefox()
+    	sel.get('https://en.wikipedia.org/wiki/Main_Page')
+
+		commons_link = sel.find_element_by_xpath('//a[text()="Commons"]')
+		self.assert(commons_link.get_attribute('href'), 'https://commons.wikimedia.org/')
 
 if __name__ == '__main__':
     unittest.main()
